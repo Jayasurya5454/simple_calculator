1fc5b9e24c05e4067503378f5fbf131786648530
unknown <jayasurya55@gmail.com>
2023-03-01 10:32:59 +0530

    calculator

---
+++ calculator.js
@@ -0,0 +1,75 @@
+function add(){
+    var a=document.getElementById("atxt").value;
+     a=Number(a);
+    var b=document.getElementById("btxt").value;
+    b=Number(b);
+
+    var c;
+    c=a+b;
+   
+    document.getElementById("clabel").innerHTML=" RESULT  =" + c;
+    
+ }
+ function sub(){
+    var a=document.getElementById("atxt").value;
+     a=Number(a);
+    var b=document.getElementById("btxt").value;
+    b=Number(b);
+
+    var c;
+    c=a-b;
+   
+    document.getElementById("clabel").innerHTML=" RESULT  =" + c;
+
+ }
+ function mult(){
+    var a=document.getElementById("atxt").value;
+     a=Number(a);
+    var b=document.getElementById("btxt").value;
+    b=Number(b);
+
+    var c;
+    c=a*b;
+   
+    document.getElementById("clabel").innerHTML=" RESULT  =" + c;
+
+ }
+ function div(){
+    var a=document.getElementById("atxt").value;
+     a=Number(a);
+    var b=document.getElementById("btxt").value;
+    b=Number(b);
+
+    var c;
+    c=a/b;
+   
+    document.getElementById("clabel").innerHTML=" RESULT  =" + c;
+
+ }
+ function mod(){
+    var a=document.getElementById("atxt").value;
+     a=Number(a);
+    var b=document.getElementById("btxt").value;
+    b=Number(b);
+
+    var c;
+    c=a%b;
+   
+    document.getElementById("clabel").innerHTML=" RESULT  =" + c;
+
+ }
+ function pow(){
+    var a=document.getElementById("atxt").value;
+     a=Number(a);
+    var b=document.getElementById("btxt").value;
+    b=Number(b);
+
+    var c;
+    c=a**b;
+   
+    document.getElementById("clabel").innerHTML=" RESULT  =" + c;
+
+ }
+
+
+ 
